426a61a2b10c90578cef2008fdb1c711f516b161
your username <amalalmomani19@gmail.com>
2020-06-06 10:37:08 +0300

    resubmit lab-07 2

--- __tests__/500.test.js
+++ __tests__/500.test.js
@@ -1,5 +1,4 @@
 const {server} = require('../lib/server.js');
-
 const supertest = require('supertest');
 const mochServer = supertest(server);
 describe('500 middleware module',()=>{
@@ -7,6 +6,5 @@ describe('500 middleware module',()=>{
     return mochServer.get('/api/v1/categories').then((result)=>{
       expect(result.status).toBe(500);
     }).catch(e=>{});
-
   });
 });
